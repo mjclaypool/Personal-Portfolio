265fa830a661262bbfdbd2b282f9f9592d138162
mjclaypool <mallory.claypool@gmail.com>
2024-08-09 14:21:24 -0400

    Update Drawer.jsx for updated routing

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -16,11 +16,11 @@ export default function Drawer( {onDone}) {
         <button type="button" aria-label="navigation-menu-close" onClick={handleCloseDrawer}>
           <img src={closeIcon} alt="Freepik close icon - Flaticon" />
         </button>
-        <Link to='/Personal-Portfolio/' id='header-title' className="drawer-elem" onClick={handleCloseDrawer}>Home</Link>
+        <Link to='/' id='header-title' className="drawer-elem" onClick={handleCloseDrawer}>Home</Link>
         <hr/>
-        <HashLink to="/Personal-Portfolio/#projects" className="drawer-elem" onClick={handleCloseDrawer}>Projects</HashLink>
-        <HashLink to="/Personal-Portfolio/#exercises" className="drawer-elem" onClick={handleCloseDrawer}>Exercises</HashLink>
-        <HashLink to="/Personal-Portfolio/#about" className="drawer-elem" onClick={handleCloseDrawer}>About</HashLink>
+        <HashLink to="/#projects" className="drawer-elem" onClick={handleCloseDrawer}>Projects</HashLink>
+        <HashLink to="/#exercises" className="drawer-elem" onClick={handleCloseDrawer}>Exercises</HashLink>
+        <HashLink to="/#about" className="drawer-elem" onClick={handleCloseDrawer}>About</HashLink>
       </div>
     </Modal>
   )

43c182b88a9b5bd9d1ec2c4a20b0a8318dc53e98
mjclaypool <148149089+mjclaypool@users.noreply.github.com>
2025-10-07 09:36:16 -0400

    Update About section responsive design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,18 +18,18 @@ export default function About() {
       <div className="flex flex-col font-bold p-8 pb-16 xl:px-24 lg:max-w-[1600px]">
         <h2 className="text-3xl self-center py-6" id='about'>About</h2>
         <div className="flex flex-col self-center max-w-full">
-          <menu className="flex justify-between gap-2 lg:max-w-[625px] lg:ml-10">
+          <menu className="grid grid-cols-2 lg:grid-cols-4 gap-4 lg:max-w-[650px] lg:ml-10">
             {caseStudyCtx.aboutTabs.map(tab => (
-              <li key={tab.tabName} className="relative flex-1 text-center hover:text-p-ice-blue focus:text-p-ice-blue">
+              <li key={tab.tabName} className="relative text-center items-center py-1 px-2 hover:text-p-ice-blue focus:text-p-ice-blue">
                 <button
                   type='button'
                   onClick={() => handleSelectedTab(tab.tabName)}
-                  className={selectedTab === tab.tabName ? 'activeTab' : 'inactiveTab'}
+                  className={`${selectedTab === tab.tabName && 'activeTab'} relative w-full h-full flex items-center justify-center px-1 rounded-t-md`}
                 >
                   {tab.tabName}
                 </button>
                 {selectedTab === tab.tabName &&
-                  <motion.div layoutId="tab-indicator" className="absolute top-0 w-full h-full bg-p-glacier-blue rounded-t-md z-0" />
+                  <motion.div layoutId="tab-indicator" className="absolute top-0 left-0 w-full h-full bg-p-glacier-blue rounded-t-md z-0" />
                 }
               </li>
             ))}

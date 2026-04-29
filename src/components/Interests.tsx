42edecc5fb5e5c6937c0a70eb73357c5f21edbe4
mjclaypool <148149089+mjclaypool@users.noreply.github.com>
2026-04-29 15:57:21 -0400

    Added 3D art asset

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -4,6 +4,7 @@ import HomeSectionHeading from './HomeSectionHeading.tsx';
 import owimg from '../assets/interests--OW-fan-art.png'
 import bikeimg from '../assets/interests--bike.png'
 import cameraimg from '../assets/interests--camera.jpg'
+import drillimg from '../assets/interests--drill-press.png'
 
 export default function Interests() {
     return(
@@ -20,6 +21,15 @@ export default function Interests() {
                             aria-label="Open live site for Vintage Camera art in a new tab"
                         >
                             <img src={cameraimg} className="max-h-[280px] rounded-xl hover:shadow-card focus:shadow-card cursor-pointer" />
+                        </a>
+                    </ArtWrapper>
+                    <ArtWrapper>
+                        <a
+                            href="https://www.artstation.com/artwork/ZlkLlx"
+                            target="_blank"
+                            aria-label="Open live site for Animated Drill Press in a new tab"
+                        >
+                            <img src={drillimg} className="max-h-[280px] rounded-xl hover:shadow-card focus:shadow-card cursor-pointer" />
                         </a>
                     </ArtWrapper>
                     <ArtWrapper>
